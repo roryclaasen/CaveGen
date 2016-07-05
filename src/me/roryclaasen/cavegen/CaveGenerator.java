1a9847826d0857bf70700dfbc61426a06de5c602
Rory Claasen (At College) <roryclaasen@live.co.uk>
2016-07-05 12:59:07 +0100

    working on a circle cave type area

--- src/me/roryclaasen/cavegen/CaveGenerator.java
+++ src/me/roryclaasen/cavegen/CaveGenerator.java
@@ -30,6 +30,7 @@ public class CaveGenerator {
 	}
 
 	public void generate(CaveConfig config) {
+		Random random = new Random(SEED);
 		if (config.doDebugOutput()) System.out.println("Creating level with seed: " + this.SEED);
 		noise = new OpenSimplexNoise(this.SEED);
 		if (config.doDebugOutput()) System.out.println("Setting tiles");
@@ -39,7 +40,30 @@ public class CaveGenerator {
 				if (value > -config.getCaveRange() && value < config.getCaveRange()) tiles[x + y * WIDTH] = 1;
 			}
 		}
+		for (int i = 0; i < 5; i++) {
+			boolean valid = false;
+			int x = 0, y = 0, size = 15;
+			while (!valid) {
+				x = random.nextInt(WIDTH);
+				y = random.nextInt(HEIGHT);
+				valid = tiles[x + y * WIDTH] == 1;
+			}
+			placeCave(x, y, size + random.nextInt(size));
+		}
 		if (config.doDebugOutput()) System.out.println("Done!");
+	}
+
+	private void placeCave(int sx, int sy, int size) {
+		for (int x = sx - (size / 2); x < sx + (size / 2); x++) {
+			if (x < 0 && x >= WIDTH) continue;
+			for (int y = sy - (size / 2); y < sy + (size / 2); y++) {
+				if (y < 0 && y >= HEIGHT) continue;
+				if (x + y * WIDTH > tiles.length) continue;
+				int xl = Math.abs(sx - x);
+				int yl = Math.abs(sy - y);
+				if (Math.floor(Math.sqrt((xl * xl) + (yl * xl))) <= size) tiles[x + y * WIDTH] = 1;
+			}
+		}
 	}
 
 	public int getWidth() {
